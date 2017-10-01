3e067515a5c2175b95cee863d91896018b72e537
Tony Lettkeman <wrenhal@gmail.com>
2017-10-01 15:15:14 -0500

    Intro
    Intro to my learning.  First Steps

---
+++ Hello_Intro.py
@@ -0,0 +1,10 @@
+#From Intro to Python at www.introtopython.org
+#My Code below
+
+print('Hello Python world!')
+
+message = "Hello Python world!"
+print(message)
+
+message = "Python is my favorite language!"
+print(message)
