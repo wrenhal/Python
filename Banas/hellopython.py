cac5875a43115381a4559f98f2e2122cef61ac54
Tony Lettkeman <wrenhal@gmail.com>
2017-11-05 22:40:22 -0600

    Working on Banas

--- Banas/hellopython.py
+++ Banas/hellopython.py
@@ -1,6 +1,5 @@
-import random
-import sys
-import os
+# This is from the video Python Programming by Derek Banas
+# https://www.youtube.com/watch?v=N4mEzFDjqtA
 
 print("Hello Python")
 
@@ -11,3 +10,22 @@ We can type a second line here
 '''
 name = "Tony"
 print(name)
+
+# 5 datatypes we're gonna focus on
+# Numbers, Strings, Lists, Tuples, and Dictionaries
+
+# 7 different math operators
+# + - * / % ** //
+# add, subtract, multiply, divide, modulus, exponent, floor division
+# modulus gives remainder of division
+# floor division discards the remainder, basically rounding the answer down.
+
+print("5 + 2 = ", 5+2)
+print("5 - 2 = ", 5-2)
+print("5 * 2 = ", 5*2)
+print("5 / 2 = ", 5/2)
+print("5 % 2 = ", 5 % 2)
+print("5 ** 2 = ", 5**2)
+print("5 // 2 = ", 5//2)
+
+
