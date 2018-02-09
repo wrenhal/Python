81c6e7a944ac387de7117267cd32fe741e8aec40
Tony Lettkeman <wrenhal@gmail.com>
2018-02-08 22:42:12 -0600

    Added some comments to code

--- CupofCode/ex1.py
+++ CupofCode/ex1.py
@@ -1,13 +1,15 @@
 print('Since this string is in quotes and tied to print function, python knows to "print" it on the screen')
-x = 9
-x = 15
-print(x)
-print(x+2)
-print(x**2)
-print(x/2)
-print(x//2)
+x = 9 # assigns 9 to x
+x = 15 # changes x to 15
+# None of the following lines change the value assigned to x
+# These only use the value in arithmetic.
+print(x) # 15
+print(x+2) # 17
+print(x**2) # 225
+print(x/2) # 7.5
+print(x//2) # 7 
 
-x = 'two '
+x = 'two ' # space added after the word so that there is a space between the outputs.
 y = 'three'
 
 print(x+y)
@@ -19,4 +21,4 @@ ln3 = ln1+ln2
 print(ln3)
 print(ln3+ln3)
 
-print('"this is a test"')
+print('"this is a test"') # shows the ability to print quotes in a string.  
